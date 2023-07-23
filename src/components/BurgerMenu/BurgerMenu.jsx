37d037d66dc94aef1186bedd8b57e865fad7fd0f
Maksim Zarubin <89940028777@ya.ru>
2023-07-23 07:31:14 +0400

    menu_updated

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,11 +1,16 @@
 import { useContext } from 'react';
 import './BurgerMenu.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import BurgerMenuContext from '../../context/BurgerMenuContext.jsx';
 
 function BurgerMenu() {
   const { isBurgerMenuOpen, toggleBurgerMenu } = useContext(BurgerMenuContext);
+  const navigate = useNavigate();
 
+  const handleProfileClick = () => {
+    toggleBurgerMenu();
+    navigate('/profile', { relace: true });
+  }
   return (
     <div className={isBurgerMenuOpen ? 'burger-menu' : ''}>
       <div className='burger-menu__container'>
@@ -14,8 +19,8 @@ function BurgerMenu() {
           <Link to='/' className='burger-menu__link'>Главная</Link>
           <Link to='/movies' className='burger-menu__link'>Фильмы</Link>
           <Link to='/saved-movies' className='burger-menu__link'>Сохраненные фильмы</Link>
-          <Link to='/profile' className='burger-menu__profile' />
         </div>
+        <span to='/profile' className='burger-menu__profile' onClick={handleProfileClick}></span>
       </div>
     </div>
   );
