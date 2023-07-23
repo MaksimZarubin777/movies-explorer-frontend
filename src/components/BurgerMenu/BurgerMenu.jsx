9f3ff35475e4d6da1c71d03d02b069679e194a92
Maksim Zarubin <89940028777@ya.ru>
2023-07-23 06:33:34 +0400

    diplom_front_burgermenu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -11,9 +11,9 @@ function BurgerMenu() {
       <div className='burger-menu__container'>
         <span className='burger-menu__close-button' onClick={toggleBurgerMenu}></span>
         <div className='burger-menu__links'>
-          <Link to='/' className='burger-menu__link'>Главная</Link>
-          <Link to='/movies' className='burger-menu__link'>Фильмы</Link>
-          <Link to='/saved-movies' className='burger-menu__link'>Сохраненные фильмы</Link>
+          <Link to='/' className='burger-menu__link' onClick={toggleBurgerMenu}>Главная</Link>
+          <Link to='/movies' className='burger-menu__link' onClick={toggleBurgerMenu}>Фильмы</Link>
+          <Link to='/saved-movies' className='burger-menu__link' onClick={toggleBurgerMenu}>Сохраненные фильмы</Link>
         </div>
         <Link to='/profile' className='burger-menu__profile' />
       </div>
