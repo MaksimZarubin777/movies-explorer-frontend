95f37dd5dfd973e98d430f56779ee605bb1235f4
Maksim Zarubin <89940028777@ya.ru>
2023-07-24 22:37:49 +0400

    fixed-films_like_local

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -94,7 +94,7 @@ function MoviesCard({
       })
         .then((data) => {
           setLikedMoviesData([...likedMoviesData, data.data]);
-          localStorage.setItem('localSavedMovies', JSON.stringify(likedMoviesData));
+          localStorage.setItem('localSavedMovies', JSON.stringify([...likedMoviesData, data.data]));
           setIsLikeClicked(true);
         })
         .catch((err) => {
@@ -105,12 +105,13 @@ function MoviesCard({
       MainApi.deleteMovie(filmToLike._id)
         .then(() => {
           setLikedMoviesData(likedMoviesData.filter((movie) => movie.movieId !== id));
-          localStorage.setItem('localSavedMovies', JSON.stringify(likedMoviesData));
+          localStorage.setItem('localSavedMovies', JSON.stringify(likedMoviesData.filter((movie) => movie.movieId !== id)));
           setIsLikeClicked(false);
         })
         .catch((err) => console.log(err));
     }
   };
+  
 
   return (
     <div className='movies-card'>
