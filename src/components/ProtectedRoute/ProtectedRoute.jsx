dc355d4b80b17c7968dc8c248dd5cf347dd4c491
Maksim Zarubin <89940028777@ya.ru>
2023-07-24 05:08:34 +0400

    fixed-always_navigate_to_/movies

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRouteElement = ({ element: Component, ...props }) => (
-  props.isLoggedIn ? <Component {...props} /> : <Navigate to="/" replace/>
+  props.isLoggedIn ? <Component {...props} /> : <Navigate to="/kura" replace/>
 );
 
 export default ProtectedRouteElement;
