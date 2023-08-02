0cbfe4c4210cca6d0b4dad66061885be5602c218
Maksim Zarubin <89940028777@ya.ru>
2023-08-02 16:54:01 +0400

    fixed_saved-films-wrong

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -124,9 +124,10 @@ function App() {
     console.log(filmsList);
     console.log(searchData);
     let searchedFilms = filmsList ? [...filmsList] : [];
+    console.log(searchedFilms);
     // Фильтрация по поисковому запросу
     searchedFilms = searchedFilms.filter((film) => film.nameRU.toLowerCase().includes(searchData));
-
+    console.log(searchedFilms);
     if (isCheckBoxActive) {
       // Фильтрация по активному состоянию чекбокса и длительности
       searchedFilms = searchedFilms.filter((film) => film.duration <= SHORT_MOVIES_DURATION);
